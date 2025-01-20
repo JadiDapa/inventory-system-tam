dd5848675c447556ca48116ff35297392a5baa80
Daffa Althaf Ridovani <daffaalthaf25@gmail.com>
2025-01-20 09:57:03 +0700

    Hello World

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,7 @@
-import Expertise from "@/components/Home/Expertise";
-import Hero from "@/components/Home/Hero";
-
 export default function Home() {
   return (
     <>
-      <Hero />
-      <Expertise />
+      <h1>Hello World</h1>
     </>
   );
 }
